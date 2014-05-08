f1f68d22118906273f3f33cb0dd4d62ba9718fea
Radu Ungureanu <radu.ungureanu1912@cti.pub.ro>
2014-05-08 23:28:23 +0300

    Added more fields to User model
    Signed-off-by: Radu Ungureanu <radu.ungureanu1912@cti.pub.ro>

--- BuddyStalkerServer/src/main/java/cs/pub/ro/pdsd/controller/UserController.java
+++ BuddyStalkerServer/src/main/java/cs/pub/ro/pdsd/controller/UserController.java
@@ -4,24 +4,22 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
-import cs.pub.ro.pdsd.model.User;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import cs.pub.ro.pdsd.model.User;
+
 @Controller
 public class UserController {
-
 	private final AtomicLong counter = new AtomicLong();
 
 	@RequestMapping(value = "/users", method = RequestMethod.GET)
 	@ResponseBody
 	public List<User> getUsers() {
 		List<User> userList = new ArrayList<>();
-		userList.add(new User(counter.incrementAndGet(), "Paul"));
-		userList.add(new User(counter.incrementAndGet(), "Rdl"));
+		userList.add(new User(counter.incrementAndGet(), "TestUser"));
 
 		return userList;
 	}
--- BuddyStalkerServer/src/main/java/cs/pub/ro/pdsd/model/User.java
+++ BuddyStalkerServer/src/main/java/cs/pub/ro/pdsd/model/User.java
@@ -3,12 +3,14 @@ package cs.pub.ro.pdsd.model;
 public class User {
 	private long id;
 	private String name;
+	private float latitude;
+	private float longitude;
 
 	public User(long id, String name) {
 		this.id = id;
 		this.name = name;
 	}
-	
+
 	public long getId() {
 		return id;
 	}
@@ -23,5 +25,21 @@ public class User {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public float getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(float latitude) {
+		this.latitude = latitude;
+	}
+
+	public float getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(float longitude) {
+		this.longitude = longitude;
 	}
 }
