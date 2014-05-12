37b43fbca8bdf5f5050f5a4f05d7d08490cac9a8
Paul Săpunaru <paul.sapunaru@gmail.com>
2014-05-12 13:44:40 +0300

    More work on the HTTP communication between client and server
    Signed-off-by: Paul Săpunaru <paul.sapunaru@gmail.com>

---
+++ BuddyStalker/src/ro/pub/cs/pdsd/buddystalker/http/client/GetUsersAsyncTask.java
@@ -0,0 +1,30 @@
+package ro.pub.cs.pdsd.buddystalker.http.client;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.http.client.ClientProtocolException;
+
+import ro.pub.cs.pdsd.buddystalker.model.User;
+import android.os.AsyncTask;
+
+public class GetUsersAsyncTask extends AsyncTask<Integer, Integer, List<User>> {
+
+	@Override
+	protected List<User> doInBackground(Integer... params) {
+		List<User> users;
+		UserClient client = new UserClient();
+
+		try {
+			users = client.getUsers();
+		} catch (ClientProtocolException e) {
+			throw new RuntimeException(e);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+
+		client.close();
+
+		return users;
+	}
+}
